735499a3b7ed69e76dbcecf31307830171e7e20c
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2025-01-09 13:41:32 +0200

    Source providers: HTTP (zip/tar)

---
+++ pylon/core/providers/source/http_tar.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+# coding=utf-8
+
+#   Copyright 2025 getcarrier.io
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+""" SourceProvider """
+
+import io
+import tarfile
+import tempfile
+
+import requests  # pylint: disable=E0401
+
+from . import SourceProviderModel
+
+
+class Provider(SourceProviderModel):  # pylint: disable=R0902
+    """ Provider model """
+
+    def __init__(self, context, settings):
+        self.context = context
+        self.settings = settings
+        #
+        self.username = self.settings.get("username", None)
+        self.password = self.settings.get("password", None)
+        self.verify = self.settings.get("verify", True)
+        #
+        self.tar_mode = self.settings.get("tar_mode", "r")
+
+    def init(self):
+        """ Initialize provider """
+
+    def deinit(self):
+        """ De-initialize provider """
+
+    def get_source(self, target):
+        """ Get plugin source """
+        target_path = tempfile.mkdtemp()
+        self.context.module_manager.temporary_objects.append(target_path)
+        #
+        username = target.get("username", self.username)
+        password = target.get("password", self.password)
+        verify = target.get("verify", self.verify)
+        #
+        auth = None
+        if username is not None and password is not None:
+            auth = (username, password)
+        #
+        response = requests.get(
+            target.get("source"),
+            auth=auth,
+            verify=verify,
+        )
+        #
+        with tarfile.open(mode=self.tar_mode, fileobj=io.BytesIO(response.content)) as tar_file:
+            tar_file.extractall(target_path)
+        #
+        return target_path
+
+    def get_multiple_source(self, targets):
+        """ Get plugins source """
+        result = []
+        #
+        for target in targets:
+            result.append(self.get_source(target))
+        #
+        return result
